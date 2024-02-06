e3e735d76f0b2ca8a9867b149c371b89db92b5da
lindllgrn <lindllgrn@gmail.com>
2024-02-06 10:43:40 -0600

    Funsies making changes

--- main.py
+++ main.py
@@ -14,9 +14,32 @@ planets = (
 )
 
 
+def get_integer_input(message, min_num=0, max_num=0):
+    while True:
+
+        try:
+            user_input = int(input(message))
+
+            if min == 0 and max == 0:
+                return user_input
+            elif min_num <= user_input <= max_num:
+                return user_input
+            else:
+                print(f"\tInvalid input: please enter a number between {min_num} and {max_num}.")
+                continue
+
+        except ValueError:
+            print("\tinvalid input: please enter a number.")
+            continue
+
+def display_abs_distance(planet1_num, planet2_num):
+
 def planet_menu():
     print('=' * DASH_LENGTH)
-    print(f"{'#1 ', planets[1][1], '= ', planets[1][1], ' million miles':.<{COLUMN_LENGTH}}")
+    menu_number = 1
+    for planet, distance in planets:
+        print(f"#{menu_number} {planet:<7} = {distance:>4} million miles")
+        menu_number += 1
     print('=' * DASH_LENGTH)
 
 
